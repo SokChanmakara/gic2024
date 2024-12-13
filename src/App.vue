d77e2a9bc0ddaedc90c9ca317442042fbdc24f82
SokChanmakara <sokchanmakara04@gmail.com>
2024-12-14 01:08:35 +0700

    TP5: Finish step 1

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { RouterLink,RouterView} from 'vue-router'
+
+</script>
+
+<template>
+  <div class="header">
+    <h1>Header</h1>
+    <div class="btn-container">
+      <router-link to="/Page1" class="btn" active-class="active">Page1</router-link>
+      <router-link to="/Page2" class="btn" active-class="active">Page2</router-link>
+      <router-link to="/Page3" class="btn" active-class="active">Page3</router-link>
+    </div>
+  </div>
+  <router-view></router-view>
+
+  <div class="footer">
+    <h1>Footer</h1>
+  </div>
+</template>
+
+<style scoped>
+.header {
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem;
+}
+
+.btn-container {
+    display: flex;
+    justify-content: space-evenly;
+    padding:1rem;
+    gap: 15px;
+}
+.btn{
+  text-decoration: none;
+  font-size:20px;
+  color:black;
+}
+.footer {
+    display: flex;
+    padding: 1rem;
+    justify-content: center;
+    margin-top: auto;
+}
+.active{
+  color:red;
+}
+</style>
---
+++ src/views/Page2.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <div class="sidebar">
+            <h1>Menu</h1>
+            <ul>
+                <li><a href="#">Section 1</a></li>
+                <li><a href="#">Section 2</a></li>
+                <li><a href="#">Section 3</a></li>
+                <li><a href="#">Section 4</a></li>
+            </ul>
+        </div>
+        <div class="content">
+            <h1>Welcome to Page 2</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .container{
+        display:flex;
+        justify-content: space-between;
+        align-content: center;
+    }
+    .sidebar{
+        padding:1rem;
+    }
+    li,
+    li > a{
+        text-decoration: none;
+        list-style: none;
+        font-family:Verdana, Geneva, Tahoma, sans-serif;
+        font-size:24px;
+        color:rgb(53, 52, 52);
+        font-weight:300 ;
+    }
+    .content{
+        display:flex;
+        position:absolute;
+        right:45%;
+        top:50%;
+        
+    }
+</style>
